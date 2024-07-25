64e356de6177fff3c2a222992f309524faa5d676
KOOKOKOK <50746384+KOOKOKOK@users.noreply.github.com>
2024-07-25 09:30:54 +0800

    Update transform.py

--- utils/transform.py
+++ utils/transform.py
@@ -5,8 +5,7 @@ import numpy as np
 
 class NormalizeImgGradAngle(ImageOnlyTransform):
     """Normalization is applied by the formula: `img = (img - mean * max_pixel_value) / (std * max_pixel_value)`
-        照着官方的类修改的使其能够对一个4维的输入包含图片梯度方向的矩阵进行归一化
-        使用梯度方向是因为如果使用梯度值不好归一化,不知道最大值和最小值,而反正切有确定的域
+       
     Args:
         mean (float, list of float): mean values
         std  (float, list of float): std values
@@ -33,10 +32,10 @@ class NormalizeImgGradAngle(ImageOnlyTransform):
         self.max_pixel_value = max_pixel_value
 
     '''
-    description: 将先在通道上分离然后再分别归一化,最后在通道上叠加在一起
+    description: 
     param {*} self
-    param {*} image[:,:,image,gradAngel],一般image都是uint8,为了迎合
-    angle就统一为float
+    param {*} image[:,:,image,gradAngel]
+    angle float
     param {object} params
     return {*}
     '''
@@ -48,7 +47,7 @@ class NormalizeImgGradAngle(ImageOnlyTransform):
         image,gradAngle = image[:,:,0:3],image[:,:,3:]
         image = F.normalize(image, self.mean, self.std, self.max_pixel_value)
         gradAngle = (gradAngle - min_value) / (max_value - min_value)
-        # 对齐维度
+        # 
         if len(gradAngle.shape) != len(image.shape):
             gradAngle = np.expand_dims(gradAngle,axis=2)
         
@@ -59,8 +58,4 @@ class NormalizeImgGradAngle(ImageOnlyTransform):
         return ("mean", "std", "max_pixel_value")
     
 
-if __name__ == '__main__':
-    trans = NormalizeImgGradAngle()
-    t = np.random.randn(640,640,4)
-    out = trans(image=t)['image']
-    print(out.shape)
+

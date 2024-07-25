f9802456761cd8a68253540fb9d15e4c87f76482
KOOKOKOK <50746384+KOOKOKOK@users.noreply.github.com>
2024-07-25 09:29:34 +0800

    Update cusnet.py

--- model/cusnet.py
+++ model/cusnet.py
@@ -45,11 +45,11 @@ class CusPartialBlock(nn.Module):
 
 class DetailBranchBlock(nn.Module):
     '''
-    description: DetailBranch 下采样二倍
+    description:  
     param {*} self
-    param {*} inChannels 输入通道数
-    param {*} outChannels 输出通道数
-    param {*} repeat 第二个卷积操作重复次数
+    param {*} inChannels 
+    param {*} outChannels
+    param {*} repeat 
     return {*}
     '''
     def __init__(self,inChannels,outChannels,repeat) -> None:
@@ -70,10 +70,10 @@ class DetailBranchBlock(nn.Module):
 class StemBlock(nn.Module):
 
     '''
-    description: Semantic Branch 所需要的第一个块 下采样四倍
+    description:  
     param {*} self
-    param {*} inChannels 输入维度
-    param {*} outChannels 输出维度
+    param {*} inChannels 
+    param {*} outChannels 
     return {*}
     '''
     def __init__(self,inChannels,outChannels) -> None:
@@ -140,10 +140,10 @@ class CusBlock(nn.Module):
     
 class DepthwiseConv2d(nn.Module):
     '''
-    description: 深度卷积
+    description: 
     param {*} self
-    param {*} in_channels 输入通道数和分组数groups保持一致
-    param {*} out_channels 同时输出通道数要能够整除groups
+    param {*} in_channels 
+    param {*} out_channels 
     param {*} kernel_size
     param {*} stride
     param {*} padding
@@ -160,11 +160,11 @@ class DepthwiseConv2d(nn.Module):
         return x
 class GatherExpandBlock(nn.Module):
     '''
-    description: 改进的 MobileNetV2 中的 Mobile Inverted Bottleneck MIB
+    description:
     param {*} self
-    param {*} inChannel 当 in < out 才会 对特征图下采样二倍
+    param {*} inChannel 
     param {*} outChannel
-    param {*} expansionFactor 在输入通道数上应用一个扩展因子来增加中间层的通道数(MobileNetV2 中,MIB 模块)
+    param {*} expansionFactor 
     return {*}
     '''
     def __init__(self,inChannel,outChannel,expansionFactor) -> None:
@@ -216,7 +216,7 @@ class GatherExpandBlock(nn.Module):
 
 class SemanticBranch(nn.Module):
     '''
-    description: 由于SegHead需要对不同阶段的语义特征处理,特地在封装一次
+    description: 
     param {*} self
     param {*} channels
     param {*} repeat
@@ -236,7 +236,7 @@ class SemanticBranch(nn.Module):
         
         inChannels = channels[0]
         channels = channels[1:]
-# 论文中两个GE为一个组(两组之间会出现一次下采样，方便使用segHead才这样写)
+# 
         for c,r,e in zip(channels,repeat,expandRatio):
             if r > 1:
                 for j in range(r):
@@ -256,18 +256,18 @@ class SemanticBranch(nn.Module):
     def forward(self,x):
         fea = []
         fea.append(self.branch[0](x))
-        # 除去第一个stem和最后一个context embbed
+        # 
         for item in self.branch[1:-1]:
-            fea.append(item(fea[-1]))#保存semantic分支的每一个大小不同的特征图为seghead准备
+            fea.append(item(fea[-1]))#
         
         semantic = fea[-1]
         out = {}
-        for i,t in enumerate(self.headApply):#选择第几个特征图存入，i与原图片缩放倍数关系为2^(i+2)
+        for i,t in enumerate(self.headApply):#
             if t:
                 out[i] = fea[i]
                 
-        # out[len(out.keys())] = semantic#out中最后一个特征图不永远不使用seghead而是直接送入Aggregation
-        out[-1] = semantic#out中最后一个特征图不永远不使用seghead而是直接送入Aggregation （将键值设置为-1）
+        # out[len(out.keys())] = semantic#out
+        out[-1] = semantic#out
 
         return out
 class ChannelAttentionModule(nn.Module):
@@ -319,7 +319,7 @@ class Mixer(nn.Module):
         return x
 class MixFormer(nn.Module):
     '''
-    description: detail与semantic汇聚层
+    description: 
     param {*} self
     param {*} inChannel
     param {*} outChannel
@@ -337,13 +337,13 @@ class MixFormer(nn.Module):
         self.up4Ratio = nn.Upsample(scale_factor=4,mode='bilinear')
         self.lastConv = Conv2dNormActivation(in_channels=outChannel,out_channels=outChannel,kernel_size=3,stride=1,padding=1,norm_layer=nn.BatchNorm2d,activation_layer=None)
     def forward(self,d:torch.Tensor,s:torch.Tensor):
-        assert d.size()[-1] // s.size()[-1] == 4,'datail特征图的一定要比semantic的大4倍'
+        assert d.size()[-1] // s.size()[-1] == 4,'4倍'
         detail_att = self.detailProcess(d)
         semantic_att = self.semanticProcess(s)
-#以上为mixer
+#mixer
         left = d + self.toDetail(semantic_att)
         right = s  + self.toSemantic(detail_att)
-        right = self.up4Ratio(right)#将两个特征图大小调整一致
+        right = self.up4Ratio(right)#
 
         fea0 = left + right
 
@@ -354,11 +354,11 @@ class MixFormer(nn.Module):
 
 class SegHead(nn.Module):
     '''
-    description: 上采样头，用于输出最终分割结果
+    description: 
     param {*} self
     param {*} inChannel
     param {*} outChannel
-    param {*} scale 缩放比率
+    param {*} scale 
     param {int} midChannel
     return {*}
     '''
@@ -385,30 +385,30 @@ class SegHead(nn.Module):
 
 class BoostBranch(nn.Module):
     '''
-    description:  辅助计算疯损失的分支，检测时不使用
+    description: 
     param {*} self
     param {*} inChannel 16
     param {*} outChannel 128
-    param {*} minScale 模型semantic分支中第一次下采样较原图片缩小的比例 4
-    param {*} classes 分类数
-    param {*} midChannel 一个列表确定是否使用内部维度提升 0表示不需要
-    param {*} headApply 需要计算哪一个semantic分支 (一个bool列表)
+    param {*} minScale 
+    param {*} classes 
+    param {*} midChannel 
+    param {*} headApply 
     return {*}
     '''
     def __init__(self,inChannel,outChannel,minScale,classes,midChannel=[128,128,128,128],headApply=[True,True,True,True]) -> None:
         super().__init__()
         
-        # 获取分支的输入通道和缩放比
+        # 
         
         cAndS = []#len = 4(channel,scale)
-        cAndS.append((inChannel,minScale))#minScale=4 论文中semantic第一次下采样比率
+        cAndS.append((inChannel,minScale))#minScale=4 
         while cAndS[-1][0] != outChannel:
             cAndS.append((cAndS[-1][0]*2,cAndS[-1][1]*2))
         
-        assert len(cAndS) == len(headApply) and len(midChannel) == len(headApply),'headApply的长度需要与cAndS一样'
+        assert len(cAndS) == len(headApply) and len(midChannel) == len(headApply),'headApply'
         self.branch = nn.ModuleDict()
 
-# 保证boost每个分支的键值与对应semantic尺度的键值一样
+# 
         for i,(c,s) in enumerate(cAndS):
             self.branch[str(i)] = SegHead(inChannel=c,outChannel=classes,scale=s,midChannel=midChannel[i]) if headApply[i] else nn.Identity()
     
@@ -424,14 +424,14 @@ class BoostBranch(nn.Module):
 
 class EBTNet(nn.Module):
     '''
-    description: 分割模型BiSeNetV2
+    description: 
     param {*} self
-    param {*} inChannel 输入图像的通道数
-    param {*} outChannel 模型最高特征深度
-    param {*} classes 分类数
-    param {*} stemChannel stem模块的输出特征通道数
-    param {*} headApply 训练时使用哪些分支 一个bool列表 一共有4个可选分支
-    return {*} 如果是train模式 返回一个辅助训练分支的分割结果和一个检测时的分割结果,如果eval则没有分支结果
+    param {*} inChannel 
+    param {*} outChannel 
+    param {*} classes 
+    param {*} stemChannel stem
+    param {*} headApply 
+    return {*} 
     '''
     def __init__(self, inChannel,classes,outChannel=128,stemChannel=16,headApply=[True,True,True,True],
                  detailBranchChannels=[64,64,128],semanticBranchChannels=[16,32,32,64,64,128,128]) -> None:
@@ -449,16 +449,16 @@ class EBTNet(nn.Module):
 
         self.detailBranch = self._loadDetailBranch(channels=detailBranchChannels)
         self.semanticBranch = self._loadSemanticBranch(headApply=headApply,channels=semanticBranchChannels,
-                                                       repeat=[1,3,1,3,1,5])#stem块的通道还是写死的
+                                                       repeat=[1,3,1,3,1,5])#stem
         self.MixFormer = MixFormer(inChannel=outChannel,outChannel=outChannel)
         self.segHead = SegHead(inChannel=outChannel,outChannel=classes,scale=8)
         self.boostBranch = BoostBranch(inChannel=stemChannel,outChannel=outChannel,minScale=4,classes=classes)
         # print('done')
     '''
-    description: 加载detailBranch分支
+    description: 
     param {*} self
-    param {*} channels 每个块输出特征图的通道数
-    param {*} repeat 每个DetailBranchBlock块内部Conv2d 3X3 stride=1 pad=1 的重复的次数
+    param {*} channels
+    param {*} repeat 
     return {*}
     '''
     def _loadDetailBranch(self,channels=[64,64,128],repeat=[1,2,2]):
@@ -470,12 +470,12 @@ class EBTNet(nn.Module):
         return nn.Sequential(*l)
 
     '''
-    description: 加载semanticBranch分支
-    param {*} self _loadSemanticBranch有些参数是写死的
-    param {*} channels 每个块输出特征图的通道数
-    param {*} repeat 每个GE重复次数
-    param {*} expandRatio 在输入通道数上应用一个扩展因子来增加中间层的通道数(MobileNetV2 中,MIB 模块)
-    return {*} 如果是训练会返回一个dict[t,t]如果eval直接返回一个tensor
+    description: 
+    param {*} self _loadSemanticBranch
+    param {*} channels 
+    param {*} repeat 
+    param {*} expandRatio 
+    return {*} 
     '''
     def _loadSemanticBranch(self,channels=[16,32,32,64,64,128,128],repeat=[1,1,1,1,1,3],expandRatio=[6,6,6,6,6,6],headApply=[False,False,False,False]):
         m = SemanticBranch(inChannel=self.inChannel,outChannel=self.outChannel,\
@@ -489,7 +489,7 @@ class EBTNet(nn.Module):
         semantic = self.semanticBranch(x)#dict[int:tensor]
         
         # fea0 = self.MixFormer(detail,semantic[len(semantic.keys())-1])#dict[int:tensor]
-        fea0 = self.MixFormer(detail,semantic[-1])#dict[int:tensor] 键值-1代表是最终用来检测的那一个
+        fea0 = self.MixFormer(detail,semantic[-1])#dict[int:tensor]
         out = self.segHead(fea0)
         # out = F.interpolate(out, 640,)
 
@@ -497,7 +497,7 @@ class EBTNet(nn.Module):
             boostOut = self.boostBranch(semantic)
             return {'pred':out,'boost':boostOut}
         else:
-            return out#做eval就应当只出现最终检测结果
+            return out#
 def CUSNet(inChannels,outChannels,headApply=[True,True,True,True]):
     model = EBTNet(inChannel=inChannels,classes=outChannels,headApply=headApply)
     # model.boostBranch = nn.Identity()
